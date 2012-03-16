6add0db7f2dacb12635d0719a7588d26fb058206
Mauro <maurobuselli@gmail.com>
2012-03-15 23:21:26 -0300

    ejercicios 1-al-11 (dia2/3) terminados

--- day2/myScripts.js
+++ day2/myScripts.js
@@ -1,24 +1,24 @@
 
 
-var postFunc = function(name){
+var postFunc = function(name, divId){
 	var url = "api/dispatcher.php";
 //	console.log(name);
 	$.post(url, {service: 'welcome.hello', params: {"name": String(name)}}, 
 		function(data){
-			$("#stage").html(data);
+			$(divId).html(data);
 			//
 			//highlight the name: 
 			//
-			highName("stage", name);
+			highName(divId, name);
 			}
 		);
 		
 		// Detectar y tratar Error:
 		// (si por ej. la url no existe...)
-		$("#stage").ajaxError(
+		$(divId).ajaxError(
 			function(event, request, settings ){
 				$(this).html("<h1>Error en el llamado al servidor remoto</h1>");
-				$('#stage').css({ 'fontSize' : '10px', 'color' : 'red' });
+				$(divId).css({ 'fontSize' : '10px', 'color' : 'red' });
 			}
 		);
 }
@@ -31,15 +31,15 @@ var postFunc = function(name){
 // HighLight the name in a div.
 //
 var highName = function(divId, userName) {
-	var divHTML = $('#'+String(divId)).html();
+	var divHTML = $(divId).html();
 	var startPos = divHTML.indexOf(userName);
 	var newHhtml = divHTML.substring(0,startPos) + "<u> <b>" + divHTML.substring(startPos, (userName.lenght)) + "</b> </u>" + divHTML.slice(startPos+(userName.lenght),0);
-	$('#'+String(divId)).html(newHhtml);
+	$(divId).html(newHhtml);
 }
 
 
 
-
+/*
 //
 // Get the top of the movies
 //
@@ -56,8 +56,41 @@ var getTopMovies = function() {
 	   	}, "jsonp"
 	);
 }
+*/
 
 
-var functionExample = function() {
-	return "example asd";
+//
+// Muestra las peliculas almacenadas en el objeto "movies" en el div (pasado por parametro)...
+//
+var showMovies = function(movies, div){
+    var rta = "<ol data-role='listview' data-theme='g'>";
+    for (var i=0; movies[i]!=null; ++i) {
+            rta += "<li> <a href='#'> ";
+            rta += "<img src='" + movies[i].BoxArt.SmallUrl + "'/>";
+            rta += "<h2>" + movies[i].Name + "</h2>";
+            rta += "<h3>Year: " + movies[i].ReleaseYear + "</h4>";
+            rta += "<h4>" + movies[i].ShortSynopsis + "</h3>";
+            rta += "</a> </li>";
+    }
+    rta += "</ol>";
+    $(div).html(rta);
+    $(div).listview('refresh');
+}
+		
+//
+// Get the top of the movies
+//
+var getTopMovies = function(div) {
+        var url = "api/dispatcher.php";
+        $.ajax({
+            url: url,
+            type: "GET",
+            crossDomain: true,
+            data: { service: 'movie.getTop',params: {'name': 'nnnull'}},
+            dataType: 'json',
+            success: function(movies){
+                showMovies(movies, div);
+            }
+        });
+
 }
